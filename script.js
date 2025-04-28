96d461ed2f368f30cfb125a8d4d60ed8b45452c2
Narendran <175111990+Twenty625@users.noreply.github.com>
2025-04-28 18:50:06 +0530

    Set container height as 600px

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
-const container = document.getElementById('container');
+const container = document.getElementById("container");
 
 function createGrid(size) {
-  container.innerHTML = '';
+  container.innerHTML = "";
+  container.style.width = "600px";
+  container.style.height = "600px";
 
   for (let i = 0; i < size * size; i++) {
-    const square = document.createElement('div');
-    square.classList.add('square');
+    const square = document.createElement("div");
+    square.classList.add("square");
     container.appendChild(square);
-    
-    square.addEventListener('mouseenter', () => {
-        square.style.backgroundColor = 'black';
+
+    square.addEventListener("mouseenter", () => {
+      square.style.backgroundColor = "black";
     });
+
+    square.style.width = `${600 / size}px`;
+    square.style.height = `${600 / size}px`;
   }
 }
 
 createGrid(16);
-
-
-  
